d80e96e8fdf4ea7438c22b9066d8a0c3c0c1a950
Admin <sandhyabagale10@gmail.com>
2021-10-08 17:04:29 +0530

    Added:using method

--- src/com/bz/empwage/EmployeeWage.java
+++ src/com/bz/empwage/EmployeeWage.java
@@ -4,42 +4,47 @@ import java.util.Random;
 
 public class EmployeeWage {
 
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static int MAX_WORKING_DAYS = 20;
+	public static final int MAX_WORKING_HRS = 100;
+
 	public static void main(String[] args) {
-		System.out.println("welcome to emloyee wage computation program");
-		final int IS_PART_TIME = 1;
-		final int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 100;
-		int NO_OF_WORKING_DAYS = 20;
-		int MAX_HRS_IN_THE_MONTH = 10;
-		{
-			int empHrs = 0;
-			int totalEmpHrs = 0;
-			int totalWorkingDays = 0;
-			while (totalEmpHrs <= MAX_HRS_IN_THE_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
-				totalWorkingDays++;
-				Random random = new Random();
-				int empCheck = random.nextInt(3);
-				switch (empCheck) {
-				case IS_PART_TIME:
-					empHrs = 8;
-					break;
+		System.out.println("Welcome to the Employee Wage Computation Program !!");
+		EmployeeWageComp();
+	}
 
-				case IS_FULL_TIME:
-					empHrs = 12;
-					break;
-
-				default:
-					empHrs = 0;
-					break;
-
-				}
-
-				totalEmpHrs += empHrs;
-				System.out.println("Days" + totalWorkingDays + "EmpHrs :" + empHrs);
+	public static void EmployeeWageComp() {
+		int workingDay = 20;
+		int empHrs = 0;
+		int empwage = 0;
+		int totalWage = 0;
+		int totalWorkingHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalWorkingHrs <= MAX_WORKING_HRS && totalWorkingHrs < MAX_WORKING_DAYS) {
+			totalWorkingDays++;
+			int empcheck = (int) (Math.floor(Math.random() * 10) % 3);
+			switch (empcheck) {
+			case 2:
+				System.out.println("Employee is Part time ");
+				empHrs = 4;
+				break;
+			case 1:
+				System.out.println("Employee is Full time ");
+				empHrs = 8;
+				break;
+			default:
+				System.out.println("Employee is Absent");
+				empHrs = 0;
 			}
-			int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Total Emp Wage:" + totalEmpWage);
-
+			totalWorkingHrs += empHrs;
+			empwage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println(" Employee Wage = " + empwage);
+			totalWage = totalWage + empwage;
+			System.out.println("Total working hrs = " + totalWorkingHrs);
+			System.out.println("Total Emp wage for 20 days = " + totalWage);
 		}
+
 	}
 }
