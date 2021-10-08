6753bd8707c3cebd8c37a7fc9252b9518d75a9f4
Admin <sandhyabagale10@gmail.com>
2021-10-08 17:13:46 +0530

    Added:by using multiple companies

--- src/com/bz/empwage/EmployeeWage.java
+++ src/com/bz/empwage/EmployeeWage.java
@@ -3,7 +3,6 @@ package com.bz.empwage;
 import java.util.Random;
 
 public class EmployeeWage {
-
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
@@ -12,17 +11,17 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Employee Wage Computation Program !!");
-		EmployeeWageComp();
+		EmployeeWage company1 = new EmployeeWage();
+		EmployeeWageComp("Dmart", 20, 20, 100);
+		EmployeeWage company2 = new EmployeeWage();
+		EmployeeWageComp("Kmart", 22, 20, 70);
+		EmployeeWage company3 = new EmployeeWage();
+		EmployeeWageComp("Bridgelabz", 15, 2, 20);
 	}
 
-	public static void EmployeeWageComp() {
-		int workingDay = 20;
-		int empHrs = 0;
-		int empwage = 0;
-		int totalWage = 0;
-		int totalWorkingHrs = 0;
-		int totalWorkingDays = 0;
-		while (totalWorkingHrs <= MAX_WORKING_HRS && totalWorkingHrs < MAX_WORKING_DAYS) {
+	public static int EmployeeWageComp(String company, int empRatePerHr, int numWorkingDays, int maxHrs) {
+		int empHrs = 0, totalEmpHrs = 8, totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < MAX_WORKING_DAYS) {
 			totalWorkingDays++;
 			int empcheck = (int) (Math.floor(Math.random() * 10) % 3);
 			switch (empcheck) {
@@ -38,13 +37,12 @@ public class EmployeeWage {
 				System.out.println("Employee is Absent");
 				empHrs = 0;
 			}
-			totalWorkingHrs += empHrs;
-			empwage = empHrs * EMP_RATE_PER_HOUR;
-			System.out.println(" Employee Wage = " + empwage);
-			totalWage = totalWage + empwage;
-			System.out.println("Total working hrs = " + totalWorkingHrs);
-			System.out.println("Total Emp wage for 20 days = " + totalWage);
+			totalEmpHrs += empHrs;
 		}
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println(company + " company Total Daily Wage for days " + totalWorkingDays + " and hours "
+				+ totalEmpHrs + " is: " + totalEmpWage);
 
+		return totalEmpWage;
 	}
 }
